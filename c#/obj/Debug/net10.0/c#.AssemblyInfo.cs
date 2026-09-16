fd90dd4e4dd02e663cd5e54635250f8267d81921
Ateng-labrador <arlantengga8@gmail.com>
2026-09-17 06:10:15 +0700

    feat: menambahkan sesuatu

--- c#/obj/Debug/net10.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net10.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad8dd9f50214475fee77eda06766d49c20644508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a56448580a7b88d3eb826bc483b0bc55888032b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
